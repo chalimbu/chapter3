ddf5851f8f11fff3fe5df66fa9dc91b2f21d6d8f
Sebastian Zapata <sebastian.zapata@geniussports.com>
2023-08-04 17:26:34 -0500

    stack simple como lista linkeada y stack desde un arreglo solucion razonable

--- src/SimpleStack.java
+++ src/SimpleStack.java
@@ -1,52 +1,53 @@
+public class SimpleStack<A> {
 
-public class SimpleStack<A>{
-
-    public static void main(String args[]){
-        SimpleStack<Integer> se=new SimpleStack<Integer>();
+    public static void main(String args[]) {
+        SimpleStack<Integer> se = new SimpleStack<Integer>();
         se.push(1);
         se.push(2);
         se.push(3);
-        System.out.println(""+se.peak());
-        System.out.println(""+se.pop());
-        System.out.println(""+se.pop());
-        System.out.println(""+se.pop());
-        System.out.println(""+se.pop());
+        System.out.println("" + se.peak());
+        System.out.println("" + se.pop());
+        System.out.println("" + se.pop());
+        System.out.println("" + se.pop());
+        System.out.println("" + se.pop());
 
     }
-    private static class StackElement<A>{
+
+    private static class StackElement<A> {
         A data;
         StackElement<A> next;
-        public StackElement(A data,StackElement<A> next){
-            this.data=data;
-            this.next=next;
+
+        public StackElement(A data, StackElement<A> next) {
+            this.data = data;
+            this.next = next;
         }
     }
 
-    StackElement<A> h=null;
+    StackElement<A> h = null;
 
-    public void push(A data){
-        StackElement ne=new StackElement<A>(data,h);
-        h=ne;
+    public void push(A data) {
+        StackElement ne = new StackElement<A>(data, h);
+        h = ne;
     }
 
-    public A pop(){
-        StackElement<A> le=h;
-        if(le==null){
+    public A pop() {
+        StackElement<A> le = h;
+        if (le == null) {
             throw new RuntimeException("stack is empty");
         }
-        h=h.next;
+        h = h.next;
         return le.data;
     }
 
-    public boolean isEmpty(){
-        if(h==null){
+    public boolean isEmpty() {
+        if (h == null) {
             return true;
         }
         return false;
     }
 
-    public A peak(){
-        if(h==null){
+    public A peak() {
+        if (h == null) {
             throw new RuntimeException("stack is empty");
         }
         return h.data;
